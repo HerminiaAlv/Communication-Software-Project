18d7ab99a2da759edde059b1486d0ca9f5fa9558
Lyca <mangarinlyca@gmail.com>
2024-04-13 14:09:44 -0700

    Update Message.java

--- CommunicationApp/src/Message.java
+++ CommunicationApp/src/Message.java
@@ -83,11 +83,11 @@ public class Message implements Serializable{
 		this.chatID = chatID;
 	}
 
-		this.user = user;	public void setUser(String user){
+	public void setUser(String user){
 		this.user = user;
 	}
 
-		this.userStatus = status;	public void setUserStatus(UserStatus userStatus){
+	public void setUserStatus(UserStatus userStatus){
 		this.userStatus = userStatus;
 	}
 
