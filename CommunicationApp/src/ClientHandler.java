c86aff8df22ca9e34e1d1c7e965bbdc0a191b883
HerminiaAlv <158019155+HerminiaAlv@users.noreply.github.com>
2024-04-23 15:00:52 -0700

    Update ClientHandler.java

--- CommunicationApp/src/ClientHandler.java
+++ CommunicationApp/src/ClientHandler.java
@@ -1,73 +1,85 @@
 
-package Hello;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientHandler implements Runnable {
-    private Socket clientSocket;
-    private Server server;
-    private ObjectOutputStream out;
-    private ObjectInputStream in;
-    private String username;
-    private String fullName;  // Added field to store the full name
+    private final Socket clientSocket;
+    private final Server server;
+    private ObjectOutputStream outputStream;
+    private ObjectInputStream inputStream;
 
     public ClientHandler(Socket socket, Server server) {
         this.clientSocket = socket;
         this.server = server;
-        try {
-            this.out = new ObjectOutputStream(clientSocket.getOutputStream());
-            this.in = new ObjectInputStream(clientSocket.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void run() {
         try {
-            Credentials credentials = (Credentials) in.readObject();
+            outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            inputStream = new ObjectInputStream(clientSocket.getInputStream());
 
-            if (server.isValidCredentials(credentials)) {
-                out.writeObject("SUCCESS");
-
-                User userInfo = server.getUserInfo(credentials.getUsername());
-                this.fullName = userInfo.getName() + " " + userInfo.getLastName();  // Store the full name
-                
-                out.writeObject(fullName);  // Send user's full name to client
-
-                this.username = credentials.getUsername();
-
-                while (true) {
-                    Message message = (Message) in.readObject();
-                    if ("exit".equalsIgnoreCase(message.getContent())) {
-                        break;
-                    }
-                    server.sendMessage(message);
+            while (true) {
+                List<ServerMessage> messages = (List<ServerMessage>) inputStream.readObject();
+                for (ServerMessage message : messages) {
+                    handleServerMessage(message);
                 }
-            } else {
-                out.writeObject("FAIL");
             }
-        } catch (Exception e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public void sendMessage(Message message) {
+    private void handleServerMessage(ServerMessage message) {
+//        if (message.getType() == null) {
+//            System.out.println("Received message with null type: " + message);
+//            return;
+//        }
+
+        System.out.println("Received message type: " + message.getType());
+        MessageTypes type = message.getType();
+
+        switch (type) {
+            case LOGIN:
+                handleLoginMessage((LoginMessage) message);
+                break;
+            default:
+                System.out.println("Unhandled message type: " + message.getType());
+        }
+    }
+
+
+    private void handleLoginMessage(LoginMessage msg) {
+        // Verify the username and password
+//        if ("amiller2".equals(msg.getUsername()) && "test".equals(msg.getPassword())) {
+//            msg.setSuccess(true);
+//            System.out.println("Login successful for: " + msg.getUsername());
+//        } else {
+//            msg.setSuccess(false);
+//            System.out.println("Login failed for: " + msg.getUsername());
+//        }
+    	
+    	if (msg.getStatus() == MessageStatus.PENDING) {
+    		msg.setStatus(MessageStatus.SUCCESS);
+    		msg.setSuccess(true);
+    	}
+
+        // Send the response back to the client
+        sendMessageToClient(msg);
+    }
+
+    private void sendMessageToClient(ServerMessage message) {
         try {
-            out.writeObject(message);
+        	List<ServerMessage> toClient = new ArrayList<>();
+        	toClient.add(message);
+            outputStream.writeObject(toClient);
+            outputStream.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getFullName() {
-        return fullName;
     }
 }
