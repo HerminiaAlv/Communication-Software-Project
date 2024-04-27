c82be40e9fad09d9f0ac1315c78c88730a697522
ChristopheSan <chrisjakesanchez@gmail.com>
2024-04-26 18:16:42 -0700

    Updated low prior types

--- CommunicationApp/src/MessageTypes.java
+++ CommunicationApp/src/MessageTypes.java
@@ -6,9 +6,9 @@ public enum MessageTypes {
 	GET_LOGS, 			// IT user requesting user logs
 	UPDATE_USER, 		// IT user editing a user
 	CREATE_CHAT, 		// User creating a new chat-room
-	ADD_USERS_TO_CHAT,	// Adding a user to the chat
-	NOTIFY_USER,		// Send notification(new msgs) to the recipient
-	PIN_CHAT,			// When a user pins a chat, Server must keep a record
+	ADD_USERS_TO_CHAT,	// Adding a user to the chat - low priority 
+	NOTIFY_USER,		// Send notification(new msgs) to the recipient - low priority
+	PIN_CHAT,			// When a user pins a chat, Server must keep a record - low priority 
 	UNDEFINED			// Default type
 }
 
