778fadaf051803792ce60144e1bed920be80f178
ChristopheSan <chrisjakesanchez@gmail.com>
2024-04-26 18:33:31 -0700

    Added createdChat - this is the chat that was created from the request

--- CommunicationApp/src/CreateChatMessage.java
+++ CommunicationApp/src/CreateChatMessage.java
@@ -1,14 +1,14 @@
 import java.util.List;
 
 public class CreateChatMessage extends ServerMessage {
-    private List<String> participantIds; 
-    private String chatName; 
+    private List<String> participantIds;
+    private ChatRoom createdChat; 
+    //private String chatName; 
 
-    public CreateChatMessage(List<String> participantIds, String chatName) {
+    public CreateChatMessage(List<String> participantIds) {
        this.setType(MessageTypes.CREATE_CHAT);
        this.setStatus(MessageStatus.PENDING);
         this.participantIds = participantIds;
-        this.chatName = chatName;
     }
 
     public List<String> getParticipantIds() {
@@ -17,5 +17,17 @@ public class CreateChatMessage extends ServerMessage {
 
     public String getChatName() {
         return chatName;
+    }
+
+    // If a successful creation on the server then place chat here
+    public void setCreatedChat(ChatRoom createdChat) {
+        this.createdChat = createdChat;
+    } 
+
+    // Client will use this in it's handler method to add the new chat
+    // to the currentUser's chat list
+    // call update chatroom function
+    public ChatRoom getCreatedChat() {
+        return createdChat;
     }
 }
