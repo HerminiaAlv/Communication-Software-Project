922304999a2cbecddda06d5b2c934e61db3644c8
ChristopheSan <chrisjakesanchez@gmail.com>
2024-04-27 13:04:57 -0700

    Client GUI updated to change centerPanel on button presses

--- CommunicationApp/src/ClientGUI.java
+++ CommunicationApp/src/ClientGUI.java
@@ -62,6 +62,13 @@ public class ClientGUI extends JFrame{
     private int WIDTH = 720;
     //private static int notificationCounter; // This might change 
 
+	private JPanel centerPanel;
+	private JPanel westPanel;
+	private JPanel currentCenterPanel; // This is the panel that is currently visible on the center panel
+
+	// Possible Center Panels
+	private viewLogChatPanel logViewPanel;
+	//private ModifyUserPanel modifyUserPanel;
 	private MessagePanel mssgPanel;
 	private UserPanel userPanel;
 	private CreateNewChatPanel createNewChatPanel;
@@ -85,7 +92,7 @@ public class ClientGUI extends JFrame{
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(25, 15));
 		
-		JPanel westPanel = new JPanel();
+		westPanel = new JPanel();
 		westPanel.setBackground(new Color(21, 96, 130));
 		contentPane.add(westPanel, BorderLayout.WEST);
 		westPanel.setLayout(new GridLayout(0, 1, 10, 20));
@@ -138,8 +145,13 @@ public class ClientGUI extends JFrame{
 		chatrooms.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent e) {
 				if (!e.getValueIsAdjusting()){
-					ChatRoom selectedItem = (ChatRoom) chatrooms.getSelectedValue();
-						//String selectedItem = (String) chatrooms.getSelectedValue();	
+					//ChatRoom selectedItem = (ChatRoom) chatrooms.getSelectedValue();
+					// Need to mssgpanel.setupChatroom(selectedItem)
+					// then invoke new panel
+
+					// TODO Go back and test this when we are actually initializing chatroom objects
+					// need to test the case where mssgPanel is already currentCenterPanel.
+					invokeNewPanel(mssgPanel);
 					}
 				}
 			});
@@ -181,21 +193,15 @@ public class ClientGUI extends JFrame{
 				mainButtonPanel.add(b);
 			mainButtonPanel.add(logoutButton);
 		}
-		
 
-		
-		
-			
 		// centerPanel is where all main panels will go
 		// The following panels will go here:
 		// Inside of a chatroom panel
 		// New chat panel
 		// View Logs panel
 		// Modify / Add user panel
-		JPanel centerPanel = new JPanel();
-		//centerPanel.setBorder(new EmptyBorder(0,0,1,1));
+		centerPanel = new JPanel();
 		centerPanel.setOpaque(false);
-		//centerPanel.setBorder(new LineBorder(new Color(78, 167, 46), 1, true));
 		contentPane.add(centerPanel);
 		centerPanel.setLayout(new GridLayout(1, 0, 0, 0));
 		//Message Field
@@ -209,18 +215,30 @@ public class ClientGUI extends JFrame{
 			}
 		});
 
-		createNewChatButton.addActionListener(new ActionListener() {	// 4/26 JSN: working on bringing this panel forward to be visible
+		// Create New Chat Initialization
+		createNewChatPanel = new CreateNewChatPanel(currentUser);
+		//userPanel = new UserPanel();
+
+		// Log Viewing Panel Initialization
+		logViewPanel = new viewLogChatPanel();
+
+		// Placing objects on the center panel
+		//centerPanel.add(mssgPanel,BorderLayout.CENTER);
+		invokeNewPanel(mssgPanel);
+		
+		// Button Listeners Here
+		createNewChatButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				createNewChatPanel = new CreateNewChatPanel(currentUser);
-				centerPanel.add(createNewChatPanel,BorderLayout.CENTER);
-				JOptionPane.showMessageDialog(createNewChatButton, "Debug");
+				invokeNewPanel(createNewChatPanel);
 			}																
 		});
-		createNewChatPanel = new CreateNewChatPanel(currentUser);
-		userPanel = new UserPanel();
-		//centerPanel.add(mssgPanel,BorderLayout.CENTER);
-		centerPanel.add(userPanel, BorderLayout.CENTER);	
-	}
+		viewLogsButton.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				invokeNewPanel(logViewPanel);
+			}																
+		});
+		
+	} // End Constructor 
 
     public void updateMessagePanel(String message) {
         SwingUtilities.invokeLater( new Runnable() {
@@ -228,6 +246,42 @@ public class ClientGUI extends JFrame{
 				mssgPanel.getTextMessages().append(message + "\n");
 			}
 		}); 
+	}
+
+	public void invokeNewPanel(JPanel panel) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					placePanelOnCenter(panel);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Removes the currentCenterPanel and places a new one.
+	 * 
+	 * @param toPlace	This is the panel you want to put on the centerPanel.
+	 *               	When you press a button to get a new view call this method
+	 *                  with the associated panel.
+	 */
+	private void placePanelOnCenter(JPanel toPlace) {
+		// Check if the panel toPlace is the same panel
+		if (toPlace == currentCenterPanel)
+			return; // do nothing
+
+		if (currentCenterPanel == null)
+			currentCenterPanel = toPlace;
+		else {
+			centerPanel.remove(currentCenterPanel);
+			currentCenterPanel = toPlace;
+		}
+		// place the new one
+		centerPanel.add(currentCenterPanel);
+		centerPanel.revalidate();  
+		centerPanel.repaint();     
 	}
 
 
