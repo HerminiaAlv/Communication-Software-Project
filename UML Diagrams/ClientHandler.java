5e9e387c04e95a1dd739de8f6988f9acc5ae700e
jsnopm <123318182+jsnopm@users.noreply.github.com>
2024-04-29 20:34:44 -0700

    Add files via upload

---
+++ UML Diagrams/ClientHandler.java
@@ -0,0 +1,156 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientHandler implements Runnable {
+    private final Socket clientSocket;
+    private final Server server;
+    private ObjectOutputStream outputStream;
+    private ObjectInputStream inputStream;
+
+    public ClientHandler(Socket clientSocket, Server server) {
+        this.clientSocket = clientSocket;
+        this.server = server;
+        
+        try {
+            this.outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            this.inputStream = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException e) {
+            System.out.println("Error setting up stream: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            while (true) {
+                List<ServerMessage> messages = (List<ServerMessage>) inputStream.readObject();
+                for (ServerMessage message : messages) {
+                     
+                    switch (message.getType()) {
+                        case LOGIN:
+                            server.handleLogin((LoginMessage) message, outputStream);
+                            sendMessageToClient(message);
+                            break; 
+                        case LOGOUT:
+                        	server.handleLogout((LogoutMessage) message);
+                            break;
+                        case CHAT_MESSAGE:
+                        	server.handleChatMessage((ChatMessage) message);
+                            //sendMessageToClient(message);
+                            break;
+                        case UPDATE_USER:
+                        	server.handleUpdateUser((UpdateUserMessage) message);
+                            break;
+                        case CREATE_CHAT:
+                        	server.handleCreateChat((CreateChatMessage) message);
+                            break;
+                        case ADD_USERS_TO_CHAT:
+                        	server.handleAddUsersToChat((AddUsersToChatMessage) message);
+                            break;
+                        case NOTIFY_USER:
+                        	server.handleNotifyUser((NotifyMessage) message);
+                            break;
+                        case PIN_CHAT:
+                        	server.handlePinChat((PinChatMessage) message);
+                            break;
+                        default:
+                            System.out.println("Received undefined message type.");
+                            break;
+                    }
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error reading from client: " + e.getMessage());
+        } 
+        finally {
+            closeConnection();
+           }
+        //     outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+        //     inputStream = new ObjectInputStream(clientSocket.getInputStream());
+
+        //     while (true) {
+
+        //     }
+        // } catch (IOException | ClassNotFoundException ex) {
+        //     ex.printStackTrace();
+        // }
+
+        
+    }
+
+    private void handleServerMessage(ServerMessage message) {
+//        if (message.getType() == null) {
+//            System.out.println("Received message with null type: " + message);
+//            return;
+//        }
+
+        System.out.println("Received message type: " + message.getType());
+        MessageTypes type = message.getType();
+
+        switch (type) {
+            case LOGIN:
+                //System.out.println("Login message received: " + message.getMessage());
+                //handleLoginMessage((LoginMessage) message);
+                break;
+            case CHAT_MESSAGE:
+            if (message instanceof ChatMessage) {
+                ChatMessage chatMessage = (ChatMessage) message;
+                System.out.println("Chat message received " + chatMessage.getMessage());
+                server.broadcastMessage(chatMessage);
+            } else {
+                System.out.println("Message is not a ChatMessage instance.");
+            }
+            break;
+            default:
+                System.out.println("Unhandled message type: " + message.getType());
+        }
+        
+    }
+
+    private void closeConnection() {
+    	try {
+    		if (inputStream != null) inputStream.close();
+    		if (outputStream != null) outputStream.close();
+    		if (clientSocket != null) clientSocket.close();
+    	} catch (IOException e) {
+    		System.out.println("Error closing connection: " + e.getMessage());
+            }
+            
+    	}
+
+//     private void handleLoginMessage(LoginMessage msg) {
+//         // Verify the username and password
+// //        if ("amiller2".equals(msg.getUsername()) && "test".equals(msg.getPassword())) {
+// //            msg.setSuccess(true);
+// //            System.out.println("Login successful for: " + msg.getUsername());
+// //        } else {
+// //            msg.setSuccess(false);
+// //            System.out.println("Login failed for: " + msg.getUsername());
+// //        }
+
+//     }
+
+    // For chat messages
+    public void handleChatMessage(ChatMessage chatMessage) {
+
+        // Send the message to all clients
+        server.broadcastMessage((ServerMessage) chatMessage);
+        sendMessageToClient(chatMessage);
+    }
+
+    private void sendMessageToClient(ServerMessage message) {
+        try {
+        	List<ServerMessage> toClient = new ArrayList<>();
+        	toClient.add(message);
+            outputStream.writeObject(toClient);
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ UML Diagrams/viewLogChatPanel.java
@@ -0,0 +1,213 @@
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Panel;
+import java.awt.ScrollPane;
+import java.awt.FlowLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+
+import java.awt.GridLayout;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.AbstractListModel;
+
+public class viewLogChatPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private Client client;
+//	private Map<String,String> users;		// Username, display name
+	private List<User> users;
+	private User currentUser;
+	private ChatRoom chat;
+	private ClientGUI mainGUI;
+	private DefaultListModel chatData;
+	private JPanel rightSplit;
+	
+	// Access all keys to the map and fill list with the values
+	
+	public viewLogChatPanel() {
+		client = new Client();
+		
+		setForeground(new Color(135, 206, 250));
+		setBackground(new Color(21, 96, 130));
+		setLayout(null);
+		
+		users = client.generateUsers(5, 10);
+		currentUser = users.get(0);
+		
+		// Panel for text messages box
+//		JPanel chatListBorder = new JPanel();
+//		chatListBorder.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46)));
+//		chatListBorder.setBounds(3, 30, 431, 360);
+//		add(chatListBorder);
+//		chatListBorder.setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setResizeWeight(0.5);
+		splitPane.setBounds(3, 31, 431, 359);
+		
+		
+		DefaultListModel userData = new DefaultListModel();
+        for (User user : users) {
+        	userData.addElement(user);
+        }
+       
+		
+//		AbstractListModel<String> userListModel = new AbstractListModel<String>() {
+//            @Override
+//            public int getSize() {
+//                return userData.size();
+//            }
+//
+//            @Override
+//            public String getElementAt(int index) {
+//                return userData.get(index);
+//            }
+//        };
+  
+		JList userList = new JList<User>(userData);
+		
+		// Testing right pane
+		splitPane.setRightComponent(userList);
+		
+		
+		
+		
+		
+//		userList.setSelectedIndex(0);
+		//rightSplit = new JPanel();
+		chatData = new DefaultListModel();
+//		for (ChatRoom room : currentUser.getChats())
+//				chatData.addElement(room);
+		
+		JList<ChatRoom> chatrooms = new JList(chatData);
+		//rightSplit.add(chatrooms);
+		
+		userList.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+				LogMessage message = new LogMessage(currentUser.getUsername());
+				client.sendMessageToServer(message);
+				new Thread(() -> client.sendMessageToServer(message));
+				
+				if (!e.getValueIsAdjusting()) {
+					currentUser = (User) userList.getSelectedValue();
+					
+					splitPane.setRightComponent(chatrooms);
+				}
+			}
+		});
+		
+
+//		
+//		chatrooms.addListSelectionListener(new ListSelectionListener() {
+//			public void valueChanged(ListSelectionEvent e) {
+//				chat = chatrooms.getSelectedValue();
+//			}
+//		});
+		
+		splitPane.setLeftComponent(userList);
+		splitPane.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46)));
+		add(splitPane);
+		
+		//add(scrollPane);
+		// Panel for participants
+		JPanel promptBorder = new JPanel();
+		promptBorder.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46)));
+		promptBorder.setBounds(3, 1, 431, 25);
+		add(promptBorder);
+		promptBorder.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea promptText = new JTextArea();
+		promptText.setText("Select chat to view\r\n");
+		promptText.setToolTipText("List of current room's members.");
+		promptText.setEditable(false);
+		promptText.setFont(new Font("Monospaced", Font.PLAIN, 12));
+		
+		promptBorder.add(promptText, BorderLayout.CENTER);
+		
+		// Panel for send button... putting button inside a border makes modifications easy!
+//				JPanel cancelBorder = new JPanel();
+//				cancelBorder.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46)));
+//				cancelBorder.setBounds(353, 392, 81, 38);
+//				add(cancelBorder);
+//				cancelBorder.setLayout(new BorderLayout(0, 0));
+//				
+//				JButton btnCancel = new JButton("Cancel");
+//				btnCancel.setToolTipText("Hit button to send message...");
+//				btnCancel.setFont(new Font("Dialog", Font.BOLD, 12));
+//				cancelBorder.add(btnCancel, BorderLayout.CENTER);
+				
+				JPanel confirmBorder = new JPanel();
+				confirmBorder.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46)));
+				confirmBorder.setBounds(255, 392, 81, 38);
+				add(confirmBorder);
+				confirmBorder.setLayout(new BorderLayout(0, 0));
+				
+				JButton btnConfirm = new JButton("OK");
+				confirmBorder.add(btnConfirm, BorderLayout.CENTER);
+				btnConfirm.setToolTipText("Press OK to confirm selection");
+				btnConfirm.setFont(new Font("Dialog", Font.BOLD, 12));
+				btnConfirm.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						mainGUI.displayLoggedMessages(chat);		
+						
+						
+						
+						
+						}
+				});
+				
+		
+		
+		// Panel for Online users... Uses JLabel + JList (Might change) 
+//		JList list = new JList();
+//		list.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46)));
+//		list.setBounds(442, 38, 107, 348);
+//		add(list);
+//		
+//		JPanel usersBorder = new JPanel();
+//		usersBorder.setBounds(442, 6, 107, 25);
+//		add(usersBorder);
+//		usersBorder.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46), new Color(78, 167, 46)));
+//		usersBorder.setLayout(new BorderLayout(0, 0));
+//		
+//		JLabel lblOnlineUsers = new JLabel("Online Users");
+//		lblOnlineUsers.setFont(new Font("Monospaced", Font.PLAIN, 12));
+//		lblOnlineUsers.setHorizontalAlignment(SwingConstants.CENTER);
+//		usersBorder.add(lblOnlineUsers, BorderLayout.CENTER);
+
+
+	}
+	
+	public DefaultListModel getViewLogsChatList() {
+		return chatData;
+	}
+}
