e02b319ad353f8c254c930540e84e9b18afbf2e8
Lyca <mangarinlyca@gmail.com>
2024-04-30 22:46:10 -0700

    Update LogoutMessage.java

--- CommunicationApp/src/LogoutMessage.java
+++ CommunicationApp/src/LogoutMessage.java
@@ -1,16 +1,3 @@
-// Proposed workflow:
-// Server receives message of type Logout
-// if .getStatus() == PENDING 
-// setStatus(MessageStatus.Success)
-// Send LogoutMessage back to the client
-//      user is logged out from their client
-
-// Server DISCONNECT CLIENT
-// REMOVE FROM ONLINE USERS
-// i think that's it...
-
-// I don't think there's any other data we need to send
-
 public class LogoutMessage extends ServerMessage {
     private MessageTypes type;
     private String username;
@@ -18,21 +5,17 @@ public class LogoutMessage extends ServerMessage {
     public LogoutMessage(String username){
     	this.type = MessageTypes.LOGOUT;
     	this.setUsername(username);
-    	
     }
-
-
+	
     public MessageTypes getType(){
         return type;
     }
-
-
-	public String getUsername() {
-		return username;
-	}
-
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    
+    public String getUsername() {
+	return username;
+    }
+	
+    public void setUsername(String username) {
+	this.username = username;
+    }
 }
