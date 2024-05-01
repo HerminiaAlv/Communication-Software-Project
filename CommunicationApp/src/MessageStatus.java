358c3238c25c574100427d220d105fc0c85e9921
Lyca <mangarinlyca@gmail.com>
2024-04-30 22:48:26 -0700

    Update MessageStatus.java

--- CommunicationApp/src/MessageStatus.java
+++ CommunicationApp/src/MessageStatus.java
@@ -2,7 +2,6 @@ public enum MessageStatus {
     // Status for chat messages
     SENT,       // Message is sent
     DELIVERED,  // Message is sent and succesfully delivered to recipient/s
-    // FAILED,     // Failed message
     READ,       // Message is read/opened by the recipient/s
 
     // Technical Status
