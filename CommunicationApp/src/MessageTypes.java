6245e70de44d25a9d90b97257f3598eccaa27470
Lyca <mangarinlyca@gmail.com>
2024-04-30 22:48:43 -0700

    Update MessageTypes.java

--- CommunicationApp/src/MessageTypes.java
+++ CommunicationApp/src/MessageTypes.java
@@ -3,29 +3,12 @@ public enum MessageTypes {
 	LOGIN,
 	LOGOUT,
 	CHAT_MESSAGE, 		// sending chat message
-	GET_LOGS, 			// IT user requesting user logs
+	GET_LOGS, 		// IT user requesting user logs
 	UPDATE_USER, 		// IT user editing a user
 	CREATE_CHAT, 		// User creating a new chat-room
 	ADD_USERS_TO_CHAT,	// Adding a user to the chat - low priority 
 	NOTIFY_USER,		// Send notification(new msgs) to the recipient - low priority
-	PIN_CHAT,			// When a user pins a chat, Server must keep a record - low priority
+	PIN_CHAT,		// When a user pins a chat, Server must keep a record - low priority
 	UPDATE_USER_LIST, 
-	UNDEFINED			// Default type
+	UNDEFINED		// Default type
 }
-
-// public class LoginMessage implements serializable {
-//	string username;
-//	string password;
-//   
-//	MessageTypes Type = Login;
-//}
-
-// public class CreateChatMessage implements serializable {
-//	string[] names;
-//	//	MessageTypes Type;
-//}
-
-// public class ChatMessage implements serializable {
-//	Message msg;
-//	MessageTypes Type;
-//}
